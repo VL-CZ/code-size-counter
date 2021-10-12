12877b2adbaef234f93ff8c63aba0f8487acaf0f
Vojtech Lengal <vojta.lengal@gmail.com>
2021-10-12 21:41:59 +0200

    added simple test for code_size_counter

---
+++ tests/test_code_size_counter.py
@@ -0,0 +1,24 @@
+import os
+import unittest
+
+from src.code_size_counter import CodeSizeCounter
+
+
+class TestCodeSizeCounter(unittest.TestCase):
+
+    def test_calculate_size_simple(self):
+        code_size_counter = CodeSizeCounter(os.path.join(_get_tests_dir(), 'simple-test-dir'), ('txt',), False,
+                                            [])
+        code_size = code_size_counter.calculate_size()
+
+        self.assertEqual(3, code_size.total_files)
+        self.assertEqual(2374, code_size.total_size)
+        self.assertEqual(35, code_size.total_lines)
+
+
+def _get_tests_dir():
+    return os.path.join(__file__, '..')
+
+
+if __name__ == '__main__':
+    unittest.main()
